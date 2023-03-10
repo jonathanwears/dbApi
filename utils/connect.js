4f96810d856562a8796be439f5ec499be4c510d4
Jona <jonathanwears@outlook.com>
2023-03-10 14:34:19 +0100

    Update connect.js

--- utils/connect.js
+++ utils/connect.js
@@ -3,11 +3,7 @@ import config from './config.js';
 
 async function connectToDb() {
   try {
-    await mongoose.connect(`mongodb+srv://${config.dbHost}${config.dbName}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      'useFindAndModify': false,
-    });
+    await mongoose.connect(`mongodb+srv://${config.dbHost}${config.dbName}`);
   } catch (err) {
     console.error(`error from connect.js: ${err}`);
   }
